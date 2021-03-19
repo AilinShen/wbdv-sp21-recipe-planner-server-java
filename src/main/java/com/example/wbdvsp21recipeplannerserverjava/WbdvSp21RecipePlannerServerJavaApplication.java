05f4e8efa73100409d274e60b5fbfd2e7427b32d
AilinShen <aln67911@gmail.com>
2021-03-18 23:04:48 -0400

    init spring boot config

---
+++ src/main/java/com/example/wbdvsp21recipeplannerserverjava/WbdvSp21RecipePlannerServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp21recipeplannerserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbdvSp21RecipePlannerServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbdvSp21RecipePlannerServerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wbdvsp21recipeplannerserverjava/WbdvSp21RecipePlannerServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp21recipeplannerserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbdvSp21RecipePlannerServerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
