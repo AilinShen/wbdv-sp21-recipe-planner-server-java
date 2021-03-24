2f88fc8a44479953d71f346e9a56e16517f7b3c6
AilinShen <aln67911@gmail.com>
2021-03-24 16:23:54 -0400

    models folder

---
+++ src/main/java/com/example/wbdvsp21recipeplannerserverjava/models/Recipe.java
@@ -0,0 +1,12 @@
+package com.example.wbdvsp21recipeplannerserverjava.models;
+
+public class Recipe {
+    private String id;
+    private String name;
+    private String creator;
+    private String ingredients;
+    private String directions;
+
+
+
+}
