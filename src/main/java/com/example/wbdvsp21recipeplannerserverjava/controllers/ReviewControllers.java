6d3d88e9d58b5982f829d1607c5dfdf39b301fbd
XinyueDang <sinreydang@gmail.com>
2021-04-09 15:14:40 -0400

    Add Review Service

---
+++ src/main/java/com/example/wbdvsp21recipeplannerserverjava/controllers/ReviewControllers.java
@@ -0,0 +1,43 @@
+package com.example.wbdvsp21recipeplannerserverjava.controllers;
+
+import com.example.wbdvsp21recipeplannerserverjava.models.Review;
+import com.example.wbdvsp21recipeplannerserverjava.services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class ReviewControllers {
+    @Autowired
+    ReviewService service;
+
+    @GetMapping("/api/reviews")
+    public List<Review> findAllReviews(){
+        return service.findAllReviews();
+    }
+
+    @PostMapping("/api/recipes/{rid}/reviews")
+    public Review createReview(
+            @PathVariable("rid") Long recipeId,
+            @RequestBody Review review
+    ) {
+        review.setRecipeId(recipeId);
+        return service.CreateReview(review);
+    }
+
+    @GetMapping("/api/users/{uid}/reviews")
+    public List<Review> findReviewForUser(
+            @PathVariable("uid") Long userId
+    ) {
+        return service.findReviewForUser(userId);
+    }
+
+    @GetMapping("/api/recipes/{rid}/reviews")
+    public List<Review> findReviewForRecipe(
+            @PathVariable("rid") Long recipeId
+    ) {
+        return service.findReviewForRecipe(recipeId);
+    }
+}
--- src/main/java/com/example/wbdvsp21recipeplannerserverjava/services/ReviewService.java
+++ src/main/java/com/example/wbdvsp21recipeplannerserverjava/services/ReviewService.java
@@ -1,20 +1,26 @@
 package com.example.wbdvsp21recipeplannerserverjava.services;
+import com.example.wbdvsp21recipeplannerserverjava.models.Recipe;
 import com.example.wbdvsp21recipeplannerserverjava.models.Review;
 import com.example.wbdvsp21recipeplannerserverjava.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class ReviewService {
     @Autowired
     ReviewRepository repository;
 
-    public Review CreateReviewForRecipe(Review review) {return repository.save(review);}
-    public Review findReviewForRecipe(Long recipeId) { return repository.findReviewForRecipe(recipeId);}
-    public Review findReviewForUser(Long userId) { return repository.findReviewForUser(userId);}
+    public Review CreateReview(Review review) {return repository.save(review);}
+    public List<Review> findAllReviews(){
+        return (List<Review>) repository.findAll();
+    }
+    public List<Review> findReviewForRecipe(Long recipeId) { return repository.findReviewForRecipe(recipeId);}
+    public List<Review> findReviewForUser(Long userId) { return repository.findReviewForUser(userId);}
 
     public Integer deleteRecipe(Long rid) {
         repository.deleteById(rid);
         return 1;
     }
-
 }
