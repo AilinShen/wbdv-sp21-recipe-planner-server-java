7f626d513ed889d8372551aa9bdde558d2552f04
XinyueDang <sinreydang@gmail.com>
2021-04-09 15:14:40 -0400

    Add Review Service

--- src/main/java/com/example/wbdvsp21recipeplannerserverjava/repositories/ReviewRepository.java
+++ src/main/java/com/example/wbdvsp21recipeplannerserverjava/repositories/ReviewRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ReviewRepository
 extends CrudRepository<Review, Long>{
     @Query(value="SELECT * FROM reviews WHERE recipe_id=:rid", nativeQuery = true)
-    public Review findReviewForRecipe(@Param("rid") Long recipeId);
+    public List<Review> findReviewForRecipe(@Param("rid") Long recipeId);
 
     @Query(value="SELECT * FROM reviews WHERE user_id=:uid", nativeQuery = true)
-    public Review findReviewForUser(@Param("uid") Long userId);
+    public List<Review> findReviewForUser(@Param("uid") Long userId);
 }
