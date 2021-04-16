8fbfee09a8181de761c70efc70473338d8c28f31
AilinShen <aln67911@gmail.com>
2021-04-16 10:22:39 -0400

    add orgin

--- src/main/java/com/example/wbdvsp21recipeplannerserverjava/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/wbdvsp21recipeplannerserverjava/security/ApplicationSecurityConfig.java
@@ -102,6 +102,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://wbdv-sp21-recipe-planner.herokuapp.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
